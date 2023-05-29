7242db4c237f3fa9873195161451043fa12ea0a2
Ankit Nayan <ankitnayan83@gmail.com>
2023-05-29 16:07:20 +0530

    commit

--- src/axiosInstance.js
+++ src/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // const BASE_URL = "http://localhost:8080/api/v1"; //devlopment
 const BASE_URL =
-  "https://job-portal-backend-dmxdw3skr-ankitnayan83.vercel.app/api/v1"; //production
+  "https://job-portal-backend-git-main-ankitnayan83.vercel.app/api/v1"; //production
 
 axios.defaults.withCredentials = true;
 export const publicRequest = axios.create({
